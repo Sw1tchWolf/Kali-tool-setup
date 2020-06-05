e86249a9681c62c5f6d8cf2aaf17d2daf132f003
Sw1tchWolf <axfactor@gmail.com>
2020-06-05 02:34:41 -0500

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -9,6 +9,7 @@ sudo apt-get dist-upgrade -y
 sudo apt-get install git -y
 sudo apt-get install python3 -y
 sudo apt-get install screen -y
+pip install --upgrade pip
 pip install ldapdomaindump
 
 cd /opt
